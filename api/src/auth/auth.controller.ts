13216a8ebf43107ac1361cdcb29ba84912190fcf
Fayozbek <154403070+fayozbekinformatika@users.noreply.github.com>
2026-07-16 09:20:35 +0500

    Update auth.controller.ts

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -21,7 +21,6 @@ export class AuthController {
         httpOnly: true,
         secure: true,
         sameSite: 'none',
-        domain: '.onrender.com',
         path: '/',
         expires: new Date(Date.now() + 1000 * 60 * 60), // 1 day
       });
